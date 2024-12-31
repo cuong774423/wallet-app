72dea13d077d0d19bfd64229e783374bcfa2f8bf
cuong774423 <cuongtranxiyor004@gmail.com>
2024-12-31 21:32:34 +0700

    update

--- src/components/Jetton.tsx
+++ src/components/Jetton.tsx
@@ -1,11 +1,8 @@
 import { useFaucetJettonContract } from "../hooks/useFaucetJettonContract";
-import { useTonConnect } from "../hooks/useTonConnect";
-import {Card, FlexBoxCol, FlexBoxRow, Button, Ellipsis} from "./styled/styled";
+import {Card, FlexBoxCol, FlexBoxRow, Ellipsis} from "./styled/styled";
 
 export function Jetton() {
-  const {connected} = useTonConnect();
-  const { jettonWalletAddress, balance, mint } = useFaucetJettonContract();
-
+  const { jettonWalletAddress, balance } = useFaucetJettonContract();
   return (
     <Card title="Jetton" style={{ backgroundColor: 'rgba(0,0,0,0.8)', color: 'white', fontFamily: 'Arial, sans-serif'}}>
       <FlexBoxCol>
@@ -19,14 +16,6 @@ export function Jetton() {
           {balance !== null ? ( <p>{balance} TON</p>
           ) : (<p>Loading...</p>)}
         </FlexBoxRow>
-        <Button
-          disabled={!connected}
-          onClick={async () => {
-            mint();
-          }}
-        >
-          Get jettons from faucet
-        </Button>
       </FlexBoxCol>
     </Card>
   );
